1abe63df35ab247275e8fb98a59e4577c954e582
David Souther <davidsouther+github@gmail.com>
2021-02-24 17:14:07 -0800

    Web form queries server

--- web/src/Form.tsx
+++ web/src/Form.tsx
@@ -1,12 +1,27 @@
-function Form() {
-    return (
-      <form>
-        <div className="mb-3">
-          <label htmlFor="address" className="form-label">Address</label>
-          <input type="text" name="address" className="form-control" />
-        </div>
-      </form>
-    );
+import {useState} from 'react';
+
+import {Address} from './types';
+
+function Form({onChange}: {onChange: (e: Address) => void}) {
+  const [address, setAddress] = useState<Address>({country: ''});
+
+  const countryChange = (country: string) => {
+    setAddress({...address, country});
+    onChange(address);
+  }
+
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlFor="country" className="form-label">Country</label>
+        <input type="text"
+                name="country"
+                className="form-control"
+                value={address.country}
+                onChange={e => countryChange(e.target.value)} />
+      </div>
+    </form>
+  );
 }
 
 export default Form;
