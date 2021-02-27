0c46aaaaccce9b5a33a13d42fd16fb73edc6f455
David Souther <davidsouther+github@gmail.com>
2021-02-27 11:13:13 -0800

    Factor out API call

--- web/src/AddressForm.tsx
+++ web/src/AddressForm.tsx
@@ -1,23 +1,30 @@
-import {useState} from 'react';
+import { useState } from "react";
 
-import Input from './form/Input';
-import Form from './form/Form';
-import {Address} from './types';
+import Input from "./form/Input";
+import Form from "./form/Form";
+import { Address } from "./types";
 
-function AddressForm(
-    {onChange, onSubmit}:
-    {onChange: (e: Address) => void, onSubmit: () => void}
-) {
-  const [address, setAddress] = useState<Address>({country: ''});
+function AddressForm({
+  onChange,
+  onSubmit,
+}: {
+  onChange: (e: Address) => void;
+  onSubmit: () => void;
+}) {
+  const [address, setAddress] = useState<Address>({ country: "" });
 
   const countryChange = (country: string) => {
-    setAddress({...address, country});
+    setAddress({ ...address, country });
     onChange(address);
-  }
+  };
 
   return (
     <Form onSubmit={onSubmit}>
-      <Input name="Country" value="address.country" valueChange={countryChange} />
+      <Input
+        name="Country"
+        value={address.country}
+        valueChange={countryChange}
+      />
     </Form>
   );
 }
