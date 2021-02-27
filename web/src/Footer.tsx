910e081fcaa62e97333ef3d9da0ea19aa94f7080
David Souther <davidsouther+github@gmail.com>
2021-02-27 10:05:29 -0800

    Start a set of Form components

--- web/src/Footer.tsx
+++ web/src/Footer.tsx
@@ -1,12 +1,10 @@
 function Footer() {
-    return (
-      <footer className="text-center">
-        Danni Beaulieu &lt;dbeauliu@&gt; &bull;
-        David Souther &lt;dsouther@&gt; &bull;
-        Peter Weisdepp &lt;weisdepp@&gt; &bull;
-        Xiaomei Xie &lt;xiex2@&gt;
-      </footer>
-    );
+  return (
+    <footer className="text-center">
+      Danni Beaulieu &lt;dbeauliu@&gt; &bull; David Souther &lt;dsouther@&gt;
+      &bull; Peter Weisdepp &lt;weisdepp@&gt; &bull; Xiaomei Xie &lt;xiex2@&gt;
+    </footer>
+  );
 }
 
 export default Footer;
