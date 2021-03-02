6cb2dfccf9741e916e9d19dfdd0e7be631e56f00
pete <weisdepp@seattleu.edu>
2021-03-01 22:32:12 -0800

    fix database connection error by switching to sqlalchemy

--- utils/db_entry_generator.py
+++ utils/db_entry_generator.py
@@ -3,21 +3,24 @@
 import mysql.connector
 import os
 import pandas
+import sqlalchemy
 
 # Use 1% of openaddress records
 RATIO = 0.01
 CURR_DIR = os.getcwd()
 
-conn = mysql.connector.connect(
-    user='root', password='password', host='127.0.0.1')
+# conn = mysql.connector.connect(
+#     user='root', password='password', host='127.0.0.1')
 
+engine = sqlalchemy.create_engine(
+    "mysql+mysqlconnector://root:password@127.0.0.1/postal")
 
 def insert_records(csv_path):
     df = pandas.read_csv(csv_path)
     # get random samples
     df = df.sample(frac=RATIO)
     # add random samples to db
-    df.to_sql('US', con=conn)
+    df.to_sql('US', con=engine, if_exists='append')
 
 
 for subdir, dirs, files in os.walk(CURR_DIR):
