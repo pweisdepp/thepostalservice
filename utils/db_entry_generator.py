2166abfd611d479c3fd5b59cebf55dd48e0e0a31
pete <weisdepp@seattleu.edu>
2021-03-01 23:03:00 -0800

    add sql datatypes to prevent truncated data

--- utils/db_entry_generator.py
+++ utils/db_entry_generator.py
@@ -3,24 +3,33 @@
 import mysql.connector
 import os
 import pandas
-import sqlalchemy
+import sqlalchemy as sa
 
 # Use 1% of openaddress records
 RATIO = 0.01
 CURR_DIR = os.getcwd()
+DATATYPES = {'ID': sa.Integer,
+             'LON': sa.FLOAT,
+             'LAT': sa.FLOAT,
+             'NUMBER': sa.VARCHAR(128),
+             'STREET': sa.VARCHAR(128),
+             'UNIT': sa.VARCHAR(128),
+             'CITY': sa.VARCHAR(128),
+             'DISTRICT': sa.VARCHAR(128),
+             'REGION': sa.VARCHAR(128),
+             'POSTCODE': sa.Integer,
+             'HASH': sa.VARCHAR(128)}
 
-# conn = mysql.connector.connect(
-#     user='root', password='password', host='127.0.0.1')
-
-engine = sqlalchemy.create_engine(
+engine = sa.create_engine(
     "mysql+mysqlconnector://root:password@127.0.0.1/postal")
+
 
 def insert_records(csv_path):
     df = pandas.read_csv(csv_path)
     # get random samples
     df = df.sample(frac=RATIO)
     # add random samples to db
-    df.to_sql('US', con=engine, if_exists='append')
+    df.to_sql('US', con=engine, if_exists='append', dtype=DATATYPES)
 
 
 for subdir, dirs, files in os.walk(CURR_DIR):
