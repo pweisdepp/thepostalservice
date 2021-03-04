14cd89f3a508399d2b96fe44e1d1e22dcb545513
pete <weisdepp@seattleu.edu>
2021-03-04 11:35:39 -0800

    add xml datatypes to fix pandas errors

--- utils/db_entry_generator.py
+++ utils/db_entry_generator.py
@@ -5,19 +5,33 @@ import os
 import pandas
 import sqlalchemy as sa
 
-# Use 1% of openaddress records
-RATIO = 0.01
-CSV_DIRECTORY = os.getcwd() + os.sep + "csvData"
-DATATYPES = {'ID': sa.Integer,
-             'LON': sa.FLOAT,
+# Use ~6% of openaddress subset (US, AU, DE) for 1 million records
+RATIO = 0.001
+# Path to CSV files
+CSV_DIRECTORY = "/home/pete/CPSC5200/csvData/"
+
+XML_DATATYPES = {'LON': 'float',
+             'LAT': 'float',
+             'NUMBER': 'str',
+             'STREET': 'str',
+             'UNIT': 'str',
+             'CITY': 'str',
+             'DISTRICT': 'str',
+             'REGION': 'str',
+             'POSTCODE': 'str',
+             'ID': 'str',
+             'HASH': 'str'}
+
+SQL_DATATYPES = {'LON': sa.FLOAT,
              'LAT': sa.FLOAT,
+             'ID': sa.VARCHAR(128),
              'NUMBER': sa.VARCHAR(128),
              'STREET': sa.VARCHAR(128),
              'UNIT': sa.VARCHAR(128),
              'CITY': sa.VARCHAR(128),
              'DISTRICT': sa.VARCHAR(128),
              'REGION': sa.VARCHAR(128),
-             'POSTCODE': sa.Integer,
+             'POSTCODE': sa.VARCHAR(128),
              'HASH': sa.VARCHAR(128)}
 
 engine = sa.create_engine(
@@ -25,11 +39,11 @@ engine = sa.create_engine(
 
 
 def insert_records(csv_path):
-    df = pandas.read_csv(csv_path)
+    df = pandas.read_csv(csv_path, dtype=XML_DATATYPES)
     # get random samples
     df = df.sample(frac=RATIO)
     # add random samples to db
-    df.to_sql('Addresses', con=engine, if_exists='append', dtype=DATATYPES)
+    df.to_sql('addresses', con=engine, if_exists='append', dtype=SQL_DATATYPES, chunksize=2048)
 
 
 for subdir, dirs, files in os.walk(CSV_DIRECTORY):
